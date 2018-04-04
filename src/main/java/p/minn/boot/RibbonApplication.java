e8fdd885ad3044709cc7441ee308629e91b06951
394286006 <freemanfreelift@gmail.com>
2018-04-04 09:18:48 +0800

    ribbon

---
+++ src/main/java/p/minn/boot/RibbonApplication.java
@@ -0,0 +1,28 @@
+package p.minn.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(scanBasePackages = { "p.minn"})
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SpringApplication.run(RibbonApplication.class, args);
+	}
+
+	@Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
